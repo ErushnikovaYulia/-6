a26bc505b9e63187c6a44d66f24cb042533a7ad2
Yulia Erusha <erushnikova.talisman@gmail.com>
2022-10-04 22:00:36 +0500

    задача 6

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход принимает число и выдает, является ли четным (делится ли оно на два без остатка).
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int i = int.Parse(Console.ReadLine());
+ if (i % 2 == 0)
+    {
+        Console.WriteLine("да четное");
+    }
+else
+    {
+        Console.WriteLine("нет, не четное");
+    }
+ Console.ReadKey();
